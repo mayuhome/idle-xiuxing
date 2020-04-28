95f32cc192b61c4be286fb37b06a5cbec8af348d
jade <mayuhome@163.com>
2020-04-28 18:13:07 +0800

    init

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import Decimal from 'break_infinity.js/break_infinity'
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss']
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
